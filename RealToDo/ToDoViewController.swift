5adba2e05bb6f082d00388425e0d56bd7b7fb15d
xxxruslanvipxxx <rysfan00@gmail.com>
2024-04-14 20:46:01 +0300

    Initial Commit

---
+++ RealToDo/ToDoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealToDo
+//
+//  Created by Руслан Забиран on 14.04.24.
+//
+
+import UIKit
+
+class ToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
